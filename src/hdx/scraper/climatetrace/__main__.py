88f3c0b4a0895b9013af728fedd00f63c32f504a
mcarans <rans@email.com>
2026-09-23 11:41:57 +1200

    add force update temporarily

--- src/hdx/scraper/climatetrace/__main__.py
+++ src/hdx/scraper/climatetrace/__main__.py
@@ -87,6 +87,7 @@ def main(
                         match_resource_order=True,
                         updated_by_script=_UPDATED_BY_SCRIPT,
                         batch=info["batch"],
+                        force_update=True,
                     )
 
 
